99f58daf62b99e0d7fcd2a17d276dfa7a150a358
Yadukrishna360 <yadukrishna628@gmail.com>
2023-06-16 23:17:24 +0530

    New changes

--- ProjectX/app.py
+++ ProjectX/app.py
@@ -4,9 +4,16 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+
 @app.route("/")
 def Home():
     return render_template("home.html")
+
+
+
+@app.route("/new")
+def New():
+    return "Nothing New"
 
 
 if __name__ == "__main__":
